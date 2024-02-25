791110af3872fefecc74d14a163cdc14b31988c3
Fawaz Khan <derpynerd@protonmail.com>
2024-02-25 18:16:40 +0530

    Updated Readme, Added LDA op, Code cleanup

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # 8085-emulator
 Emulating an 8085 processor in C++
+
+## Specifications - 
+- 64KB of memory (1024 * 64 * 8 bits)
+
+## Functional Overview -
+- Reset operation
+- Fetch -> Decode -> Execute cycle implementation
+    - Fetches current instruction from `Memory[PC]` and stores into the `IR`
+    - Decodes fetched instruction via switch-case
+    - If decoded instruction is valid, it gets executed
+
+## Opcodes Supported -
+- `RESET` processor (Gets executed on startup) -
+    - Sets initial value for all internal registers
+    - Sets all flags to `0`
+    - Sets value in accumulator to `0`
+    - Initializes all memory bits to `0`
+- `LDA (Address)` [Load Accumulator Immediate] -
+    - Fetches value to be loaded into `A` from `Memory[PC]`
+    - Loads value into `A`
+    - Sets `Z` & `S` flags according to value in `A`
+
+## Abbreviations - 
+- PC - Program Counter
+- SP - Stack Pointer
+- IR - Instruction Register
+- A - Accumulator
+- Z - Zero flag
+- S - Sign flag
--- main.cpp
+++ main.cpp
@@ -1,77 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-using Byte = unsigned char; // 8 bit
-using Word = unsigned short; // 16 bit
-
-using u32 = unsigned int; // 64 bit
-
-class Memory {
-    static constexpr u32 MAX_MEM = 1024 * 64;
-    Byte Data[MAX_MEM];
-
-    protected:
-    void Initialize(Memory& memory) {
-        for(u32 i = 0; i < MAX_MEM; ++i) {
-            memory.Data[i] = 0;
-        }
-    }
-};
-
-class CPU : private Memory {
-
-    /* Internal Registers */
-    Word SP; // Stack pointer
-    Word PC; // Program counter
-    Byte IR; // Instruction register
-
-    /* Registers */
-    Byte A; // Accumulator
-    Word WZ; // Temporary registers
-    Word BC, DE, HL; // Registers
-
-    /* Flag register */
-    Byte S : 1; // Sign flag 
-    Byte Z : 1; // Zero flag
-    Byte AC : 1; // Auxiliary Carry flag
-    Byte P : 1; // Parity flag
-    Byte C : 1; // Carry flag
-
-    /* Signals */
-    Word ResetIn : 1; // RESET CPU signal
-    Word ResetOut : 1; // RESET acknowledge - Output
-    Word HOLD : 1; // HOLD request 
-    Word HLDA : 1; // HOLD acknowledge - Output
-
-    /* Interrupts */
-    Word INTR : 1; // Interrupt request - Least priority
-    Word INTA : 1; // Interrupt acknowledge - Output
-    Word RST5_5 : 1; //  002C
-    Word RST6_5 : 1; // 0034
-    Word RST7_5 : 1; // 003C
-    Word TRAP : 1; // 	0024
-
-    public:
-    /* Reset routine */
-    void Reset(Memory& memory) {
-        PC = 0x0000; // No reset vector for 8085
-        IR = 0x00; // Default reset routine
-        SP = 0x0100; // Starting stack from random memory location
-
-        S = Z = AC = P = C = 0; // Resetting all flags 
-
-        Memory::Initialize( memory ); // Initializing all memory bits to 0
-    }
-
-};
+#include "constants.h"
+#include "exceptions.h"
+#include "memory.h"
+#include "cpu.h"
 
 int main() {
 
     CPU cpu;
     Memory memory;
 
-    cpu.Reset( memory );
-
+    cpu.Reset( memory ); // RESET CPU to base state
+    memory.setByte( 0, 0x3A );
+    memory.setByte( 1, 0b10001000 );
+    cpu.Execute( 2, memory ); // Execute instructions with 2 clock cycles
 
     return 0;
 }
