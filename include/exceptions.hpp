1754d02d355e994f980540e0132fac16fa4992bf
Fawaz Khan <derpynerd@protonmail.com>
2024-02-26 23:11:10 +0530

    OutOfCycles exception added

--- include/exceptions.hpp
+++ include/exceptions.hpp
@@ -12,3 +12,9 @@ class MemoryOutOfBounds : public std::exception {
         return "Trying to address memory outside of MAX_MEM";
     }
 };
+
+class OutOfCycles : public std::exception {
+    virtual const char* what() const throw() {
+        return "Insufficient clock cycles to execute given instruction";
+    }
+};
